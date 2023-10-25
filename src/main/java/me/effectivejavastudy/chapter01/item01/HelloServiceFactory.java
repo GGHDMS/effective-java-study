b27c5b37c308fb00db5369ca47ba6929d17825f9
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-25 17:06:37 +0900

    아이템 1. 완벽 공략 5 - 리플렉션
    • 클래스로더를 통해 읽어온 클래스 정보(거울에 반사”된 정보)를 사용하는 기술
    • 리플렉션을 사용해 클래스를 읽어오거나, 인스턴스를 만들거나, 메소드를 실행하거나, 필드의 값을 가져오거나 변경하는 것이 가능하다.
    • 언제 사용할까?
        • 특정 애노테이션이 붙어있는 필드 또는 메소드 읽어오기 (JUnit, Spring)
        • 특정 이름 패턴에 해당하는 메소드 목록 가져와 호출하기 (getter, setter)

--- src/main/java/me/effectivejavastudy/chapter01/item01/HelloServiceFactory.java
+++ src/main/java/me/effectivejavastudy/chapter01/item01/HelloServiceFactory.java
@@ -2,12 +2,14 @@ package me.effectivejavastudy.chapter01.item01;
 
 import me.whiteship.hello.ChineseHelloService;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Optional;
 import java.util.ServiceLoader;
 
 public class HelloServiceFactory {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         // 정적 팩터리 메서드를 작성하는 시점에는 반환할 객체의 클래스가 존재하지 않아도 된다.
         ServiceLoader<HelloService> loader = ServiceLoader.load(HelloService.class);
         Optional<HelloService> helloServiceOptional = loader.findFirst();
@@ -15,5 +17,11 @@ public class HelloServiceFactory {
 
         ChineseHelloService helloService = new ChineseHelloService();
         System.out.println(helloService.hello());
+
+        Class<?> aClass = Class.forName("me.whiteship.hello.ChineseHelloService");
+        Constructor<?> constructor = aClass.getConstructor();
+        HelloService o = (HelloService) constructor.newInstance();
+
+        System.out.println(o.hello());
     }
 }
