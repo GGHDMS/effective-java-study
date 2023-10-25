6908f4d2e2a5dc4efe7a50a3380bf5484078a297
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-25 16:01:30 +0900

    아이템 1. 완벽 공략 1 - 열거 타입
    상수 목록을 담을 수 있는 데이터 타입.
    특정한 변수가 가질 수 있는 값을 제한할 수 있다. 타입-세이프티 (Type-Safety)를 보장할 수 있다.
    싱글톤 패턴을 구현할 때 사용하기도 한다.

--- src/main/java/me/effectivejavastudy/chapter01/item01/Order.java
+++ src/main/java/me/effectivejavastudy/chapter01/item01/Order.java
@@ -1,5 +1,7 @@
 package me.effectivejavastudy.chapter01.item01;
 
+import java.util.Arrays;
+
 public class Order {
 
     private boolean prime;
@@ -7,6 +9,21 @@ public class Order {
     private boolean urgent;
 
     private Product product;
+
+    // 타입 세이프티
+    // 싱글톤
+    private OrderStatus orderStatus;
+
+/*
+    // 1 - 주문 받음
+    // 2 - 준비중
+    // 3 - 배송 중
+    // 4 - 배송 완료
+    private int status;
+
+    private final int PREPARING = 0;
+    private final int SHIPPED = 2;
+*/
 
     // 이름을 가질 수 있다.
     public static Order primeOrder(Product product) {
@@ -22,5 +39,19 @@ public class Order {
         order.urgent = true;
         order.product = product;
         return order;
+    }
+
+    public static void main(String[] args) {
+        Order order = new Order();
+
+        // Enum 을 key 로 사용하는 Map, Set 은 EnumMap, EnumSet 사용해야 효율적
+
+        // equals 사용하면 NullPointerException 발생한다.
+        if (order.orderStatus != OrderStatus.DELIVERED) {
+            System.out.println("not delivery");
+        }
+
+        Arrays.stream(OrderStatus.values()).forEach(System.out::println);
+
     }
 }
