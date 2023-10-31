150e5d1e0f38f86ce73f9aa073efebf6956be637
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-31 16:48:20 +0900

    아이템 3. 완벽 공략 11 - 메서드 참조
    메소드 하나만 호출하는 람다 표현식을 줄여쓰는 방법
    
    • 스태틱 메소드 레퍼런스
    • 인스턴스 메소드 레퍼런스
    • 임의 객체의 인스턴스 메소드 레퍼런스
    • 생성자 레퍼런스

---
+++ src/main/java/me/effectivejavastudy/chapter01/item03/methodreference/Person.java
@@ -0,0 +1,51 @@
+package me.effectivejavastudy.chapter01.item03.methodreference;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Person {
+
+    LocalDate birthday;
+
+    public Person() {
+
+    }
+
+    public Person(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public static int compareByAge(Person a, Person b) {
+        return a.birthday.compareTo(b.birthday);
+    }
+
+    public static void main(String[] args) {
+        List<LocalDate> dates = new ArrayList<>();
+        dates.add(LocalDate.of(1982, 7, 15));
+        dates.add(LocalDate.of(1982, 7, 15));
+        dates.add(LocalDate.of(1982, 7, 15));
+        dates.stream().map(Person::new).collect(Collectors.toList());
+
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(LocalDate.of(1982, 7, 15)));
+        people.add(new Person(LocalDate.of(2011, 3, 2)));
+        people.add(new Person(LocalDate.of(2013, 1, 28)));
+
+        people.sort(new Comparator<Person>() {
+            @Override
+            public int compare(Person a, Person b) {
+                return a.birthday.compareTo(b.birthday);
+            }
+        });
+
+        people.sort(Person::compareByAge);
+    }
+
+    public int getAge() {
+        return LocalDate.now().getYear() - birthday.getYear();
+    }
+
+}
