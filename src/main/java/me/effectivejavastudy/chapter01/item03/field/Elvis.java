5c3f8ef5f466dfd00aaa84e7c55d281067179a43
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-10-31 15:15:34 +0900

    아이템 3. 핵심 정리 1 - 생성자를 사용하는 방법 1
    첫번째 방법: private 생성자 + public static final 필드
    • 장점, 간결하고 싱글턴임을 API에 들어낼 수 있다.
    • 단점 1, 싱글톤을 사용하는 클라이언트 테스트하기 어려워진다.

---
+++ src/main/java/me/effectivejavastudy/chapter01/item03/field/Elvis.java
@@ -0,0 +1,29 @@
+package me.effectivejavastudy.chapter01.item03.field;
+
+// 코드 3-1 public static final 필드 방식의 싱글턴 (23쪽)
+public class Elvis implements IElvis {
+
+    /**
+     * 싱글톤 오브젝트
+     */
+    public static final Elvis INSTANCE = new Elvis();
+    private static boolean created;
+
+    private Elvis() {
+    }
+
+    public void leaveTheBuilding() {
+        System.out.println("Whoa baby, I'm outta here!");
+    }
+
+    public void sing() {
+        System.out.println("I'll have a blue~ Christmas without you~");
+    }
+
+    // 이 메서드는 보통 클래스 바깥(다른 클래스)에 작성해야 한다!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.INSTANCE;
+        elvis.leaveTheBuilding();
+    }
+
+}
